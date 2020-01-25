33b30852f497a5af7fa17a5f3c09f67801bab5bc
Carlos Diaz <cdiaz86@gmail.com>
2020-01-25 14:22:18 -0500

    Adds click events to buttons.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { getFunName } from '../helpers.js'
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this);
+  }
+  
   render() {
     return (
       <React.Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={ this.goToStore }>
           <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name"/>
+          <input
+            type="text"
+            ref={this.myInput}
+            required placeholder="Store Name"
+            defaultValue={ getFunName() }
+          />
           <button type="submit">Visit Store</button>
         </form>
       </React.Fragment>
