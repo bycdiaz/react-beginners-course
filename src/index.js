feee5282e2a7981419d0e6a43c45c981544bc765
Carlos Diaz <cdiaz86@gmail.com>
2020-01-25 02:09:41 -0500

    Imports style.css into index.js.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker.js'
+import StorePicker from './components/StorePicker.js';
+import './css/style.css';
 
 render(<StorePicker />, document.querySelector('#main'));
-
-// Left off on video 5 - Loading CSS into React app
