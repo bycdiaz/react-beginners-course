69dd8d42cd5e44bc76b84976265a49baf168dcd3
Carlos Diaz <cdiaz86@gmail.com>
2020-01-25 22:15:19 -0500

    Adds form data to app state.

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,28 @@ import Order from './Order.js'
 import Inventory from './Inventory.js'
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    Order: {},
+  };
+
+  addFish = (fish) => {
+    // copy initial state
+    const fishes = { ...this.state.fishes };
+    // add new fish to fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // set new fishes object to state
+    this.setState({ fishes });
+  };
+
   render() {
     return (
-      
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     );
   }
