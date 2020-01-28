a476e4ec4547559e30cde0a20f396d70121e63e9
Carlos Diaz <cdiaz86@gmail.com>
2020-01-27 19:52:46 -0500

    Adds order to localStorage.

--- src/components/App.js
+++ src/components/App.js
@@ -21,9 +21,14 @@ class App extends React.Component {
      });
   }
 
+  componentDidUpdate() {
+    localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
+  }
+
   componentWillUnmount() {
     base.removeBinding(this.ref);
   }
+
 
   addFish = fish => {
     // 1. Take a copy of the existing state
